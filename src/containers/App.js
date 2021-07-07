4738cb41b9ec820db5f5f98628a374376822ae87
Alexandru Dragos <AlexandruD@gmail.com>
2021-07-07 14:42:57 +0300

    implemented hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import 'tachyons';
 import Scroll from '../components/Scroll';
@@ -6,51 +6,40 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import ErrorBoundry from '../components/ErrorBoundry';
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      active_robots: [],
-      search_field: ''
-    }
-  }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(people => this.setState({active_robots: people})
+    .then(people => {setRobots(people)}
+    );
+  }, []) //Runs only when the list is empty.
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value)
+    }
+
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+  })
+
+  if (robots.length === 0) {
+    return <h1 className='tc'>Loading...</h1>
+  } else {
+    return (
+      <div className='tc'>
+        <h1 className='f1' id='title' >Robofriends</h1>
+        < SearchBox searchChange={onSearchChange}/>
+        < Scroll>
+          <ErrorBoundry>
+            < CardList props={filteredRobots}/>
+          </ErrorBoundry>
+        </Scroll>
+      </div>
     );
   }
-  onSearchChange = (event) => {
-    this.setState({search_field: event.target.value})
-    
-    }
-
-  render() {
-    //Can make much cleaner:
-    // const {active_robots, search_field} = this.state;
-    const filteredRobots = this.state.active_robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.search_field.toLowerCase())
-    })
-
-    if (this.state.active_robots.length === 0) {
-      return <h1 className='tc'>Loading...</h1>
-    } else {
-      return (
-        <div className='tc'>
-          <h1 className='f1' id='title' >Robofriends</h1>
-          < SearchBox searchChange={this.onSearchChange}/>
-          < Scroll>
-            <ErrorBoundry>
-              < CardList props={filteredRobots}/>
-            </ErrorBoundry>
-          </Scroll>
-        </div>
-      );
-    }
-    
-  }
-  
 }
 
 export default App;
